d7283a89e936fef3658f987e02e6c2a3e65b34f3
ubm <ubm@covertstrike.org>
2017-07-18 18:36:15 +0200

    Update watchdog.bat
    removed unused stuff, added a 4hour timer to restart every 4 hours in every case.

--- watchdog.bat
+++ watchdog.bat
@@ -1,29 +1,30 @@
 @echo off
-echo -------------------------------------------------------------------------------------
+echo -------------------------------------------------------------------
 echo Simple script to restart your miner after the word "Error" appears in the logfile
-echo -------------------------------------------------------------------------------------
+echo -------------------------------------------------------------------
 echo:
 set restartinseconds=3
-set /a counter=1
 set executable=ethminer.exe
-timeout 3
 setx GPU_FORCE_64BIT_PTR 0
 setx GPU_MAX_HEAP_SIZE 100
 setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
-CLS
 :start
-echo time() > output.log
-start /b "ETHMiner" mining.cmd
-timeout 25 >nul
+CLS
+set /a timer=0
+echo %time% > output.log
+::start miner
+start /b "ETHMiner" mining.cmd										
+timeout 25 >nul														
 :good
-timeout 10 >nul
-findstr /i "error" output.log 
+timeout 10 >nul														
+set /a timer+=10
+if %timer% gtr 14400 goto bad 										
+findstr /i "error" output.log 										
 if errorlevel 1 goto good
+:bad
 taskkill /f /im %executable%
-echo Restarted %a% times. - 
 echo Restarting Miner in %restartinseconds% seconds (%counter%)
 timeout %restartinseconds%
-set /a counter+=1
 goto start
