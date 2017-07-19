e9374947266c5c938b98721358464e844cdc1386
ubm <ubm@covertstrike.org>
2017-07-19 16:42:58 +0200

    fixed typo

--- watchdog.bat
+++ watchdog.bat
@@ -9,9 +9,9 @@ ECHO ...............................................
 ::default Pool , 3 pools at the moment so 123, i fyou want mor or less, just change to something like C:12 or C:12345, D:1 stands for Default Pool 1
 CHOICE /N /C:123 /T 10 /D 1
 ::configure your Pools here
-IF %ERRORLEVEL% ==3 set runner=http://etc-eu1.nanopool.org:18888/0xeaaecd2c2d6b27e49fa3c29f5e3d49a7f17e3ea7/1/uBm@covertstrike.org
-IF %ERRORLEVEL% ==2 set runner=http://eth-eu1.nanopool.org:8888/0x762c924a469f21a446529bd8f6b07db6bde124bf/1/ubm@covertstrike.org
-IF %ERRORLEVEL% ==1 set runner=http://eth-eu.dwarfpool.com:80/0x762c924a469f21a446529bd8f6b07db6bde124bf/GTX1060/ubm@covertstrike.org
+IF %ERRORLEVEL% ==3 set runner=http://etc-eu1.nanopool.org:18888/0xeaaecd2c2d6b27e49fa3c29f5e3d49a7f17e3ea7/1/email@miner.com
+IF %ERRORLEVEL% ==2 set runner=http://eth-eu1.nanopool.org:8888/0x762c924a469f21a446529bd8f6b07db6bde124bf/1/email@miner.com
+IF %ERRORLEVEL% ==1 set runner=http://eth-eu.dwarfpool.com:80/0x762c924a469f21a446529bd8f6b07db6bde124bf/GTX1060/email@miner.com
 ::end configuration
 color 1B
 set restartinseconds=5
@@ -27,7 +27,7 @@ color 1B
 set /a timer=0
 echo %time% > output.log
 ::start miner, pass pool to miner
-start /b "ETHMiner" mining0.cmd %runner%										
+start /b "ETHMiner" mining.cmd %runner%										
 timeout 25 >nul														
 :good
 timeout 10 >nul														
