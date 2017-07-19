dfdf10220bd311e42133ef303d7bfc54bb741923
ubm <ubm@covertstrike.org>
2017-07-19 16:41:31 +0200

    New structure
    added support for mining on different pools

--- mining.cmd
+++ mining.cmd
@@ -1,3 +1,5 @@
 @echo off
-ethminer.exe --farm-recheck 350 -U -SC 2 -SP 1 -F http://eth-eu.dwarfpool.com:80/0x762c924a469f21a446529bd8f6b07db6bde124bf 2>&1 | wtee output.log
+set target=%1
+echo mining to : %target%
+ethminer --farm-recheck 350 -SC 2 -U -F %target% --cuda-parallel-hash 4 --cuda-grid-size 8192 --cuda-block-size 128 -v 9 --cl-global-work 8192 2>&1 | wtee output.log
 exit
