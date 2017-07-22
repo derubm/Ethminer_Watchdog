9e8390a6324d773661fc3a094ec4dad26e93926e
ubm <6720202+derubm@users.noreply.github.com>
2017-07-23 01:24:56 +0200

    Update mining.cmd
    added Ansi Codepage

--- mining.cmd
+++ mining.cmd
@@ -1,5 +1,6 @@
 @echo off
 set target=%1
+chcp 65001
 echo mining to : %target%
-ethminer --farm-recheck 350 -SC 2 -U -F %target% --cuda-parallel-hash 4 --cuda-grid-size 8192 --cuda-block-size 128 -v 9 --cl-global-work 8192 2>&1 | wtee output.log
+ethminer --farm-recheck 450 -SC 2 -U -F %target% --cuda-parallel-hash 4 --cuda-grid-size 8192 --cuda-block-size 256 -v 3 2>&1 | wtee output.log
 exit
