f1d931624e49b7fd8443351c119499d5c641f1ff
ubm <ubm@covertstrike.org>
2017-07-26 15:22:41 +0200

    Update mining.cmd

--- mining.cmd
+++ mining.cmd
@@ -2,5 +2,7 @@
 set target=%1
 chcp 65001
 echo mining to : %target%
-ethminer --farm-recheck 450 -SC 2 -U -F %target% --cuda-parallel-hash 4 --cuda-grid-size 8192 --cuda-block-size 256 -v 3 2>&1 | wtee output.log
+ethminer -F %target% --farm-recheck 450 -SC 2 -U --cuda-parallel-hash 4 --cuda-grid-size 8192 --cuda-block-size 256 -v 3 2>&1 | wtee output.log
+:: Stratum below, i suggest using stratum proxy instead.
+:: ethminer -S %target% --farm-recheck 450 -SC 2 -U --cuda-parallel-hash 4 --cuda-grid-size 8192 --cuda-block-size 256 -v 3 2>&1 | wtee output.log
 exit
